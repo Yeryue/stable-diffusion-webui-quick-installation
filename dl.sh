a856f03a28f4e51785bcf91c2e2ed8c0591a2735
falcon316409 <Falcon316409@gmail.com>
2026-02-13 23:45:45 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -3,6 +3,7 @@ curl https://repo.anaconda.com/archive/Anaconda3-2025.12-1-Linux-x86_64.sh --out
 sudo chmod +x ./anaconda.sh
 sh ./anaconda.sh
 conda create -n anyname python=3.10
+sleep 1
 conda activate anyname
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 chmod +x ./stable-diffusion-webui/webui.sh
